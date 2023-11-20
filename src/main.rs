7a82e4bfec734dc56ee045343ff7ed851e17cb87
korbinian <korbinianfl98@googlemail.com>
2023-11-20 19:17:00 +0100

    add readme

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,18 @@ fn main() {
         task!(c: 2, t: 7, d: 7, p: 2),
         task!(c: 2, t: 7, d: 7, p: 1),
     ]);
-    println!("{:?}", s.solve_fpns(Numbers::Real))
+    println!("{:?}", s.solve_fpns(Numbers::Real));
+    println!("{:?}", s.solve_fpps());
+    let s = TaskSet::new(vec![
+        task!(c:  6, t: 24, d: 16, o: 3, p: 3),
+        task!(c:  4, t: 35, d: 20, o: 3, p: 2),
+        task!(c: 13, t: 40, d: 52, o: 2, p: 1),
+    ]);
+    println!("{:?}", s.solve_fpts(Numbers::Real));
+    let s = TaskSet::new(vec![
+        task!(c: 11, t: 32, d: 28, o: 2, p: 3),
+        task!(c: 10, t: 49, d: 48, o: 3, p: 2),
+        task!(c: 12, t: 83, d: 76, o: 3, p: 1),
+    ]);
+    println!("{:?}", s.solve_fpts(Numbers::Natural));
 }
